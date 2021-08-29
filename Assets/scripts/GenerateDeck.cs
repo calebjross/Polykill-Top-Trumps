cce215d83b9e6b4dd8cff483728950e6db977b1a
Caleb J. Ross <calebjross@gmail.com>
2021-08-29 16:49:50 -0500

    Initial commit

---
+++ Assets/scripts/GenerateDeck.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+public class GenerateDeck : MonoBehaviour
+{
+    #region Fields
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Polykill-Top-Trumps
+ 
